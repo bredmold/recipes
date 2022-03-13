3218736f8043b6f615c6eb0f817a249b0470b53d
Aaron Goldstein <bredmold@gmail.com>
2022-03-13 13:29:17 -0700

    Test coverage for Home component.

--- README.md
+++ README.md
@@ -29,7 +29,7 @@ To get more help on the Angular CLI use `ng help` or go check out the [Angular C
 ## TODO List
 
 * Test coverage for components
-  * Home
+  * App
 * Improve Home Component layout
 * Save and edit buttons should be on right side of screen
 * Recipe viewer should render fractions
--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,24 +1,68 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { HomeComponent } from './home.component';
+import {HomeComponent} from './home.component';
+import {RecipeService} from "../recipe.service";
+import {Recipe} from "../types/recipe";
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let recipeServiceSpy: any;
 
   beforeEach(async () => {
+    recipeServiceSpy = {
+      clearActiveRecipes: jasmine.createSpy('clearActiveRecipes'),
+      listRecipes: jasmine.createSpy('listRecipes'),
+    }
     await TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: RecipeService,
+          useValue: recipeServiceSpy
+        }
+      ],
       declarations: [HomeComponent],
     }).compileComponents();
   });
 
-  beforeEach(() => {
+  function createComponent2() {
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }
+
+  function createComponent(recipes: Recipe[]) {
+    recipeServiceSpy.listRecipes.and.returnValue(Promise.resolve(recipes));
+    createComponent2();
+  }
+
+  function createComponentFailure(error: string) {
+    recipeServiceSpy.listRecipes.and.returnValue(Promise.reject(error));
+    createComponent2();
+  }
 
   it('should create', () => {
+    createComponent([]);
     expect(component).toBeTruthy();
+  });
+
+  it('should generate an appropriate recipe link', () => {
+    createComponent([]);
+    const link = component.recipeLink('testid')
+    expect(link).toBe('recipe/testid');
+  });
+
+  it('should set the active recipe', async () => {
+    createComponent([new Recipe('title', 'desc', [], [])]);
+    await fixture.whenStable();
+
+    expect(component.allRecipes).toHaveSize(1);
+  });
+
+  it('should not fail on error', async () => {
+    createComponentFailure('test case');
+    await fixture.whenStable();
+
+    expect(recipeServiceSpy.clearActiveRecipes.calls.count()).toEqual(1);
   });
 });
