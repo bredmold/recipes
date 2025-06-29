ffa0af145ad86e941e604fe948002fc05b61c1dc
Aaron Goldstein <bredmold@gmail.com>
2025-06-28 18:14:32 -0700

    Retry 5XX errors up to three times

--- src/app/services/backend.service.ts
+++ src/app/services/backend.service.ts
@@ -1,4 +1,4 @@
-import { lastValueFrom } from 'rxjs';
+import { lastValueFrom, Observable, retry } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { environment } from '../../environments/environment';
@@ -14,16 +14,37 @@ export class BackendService {
     this.baseUrl = environment.backendUrl;
   }
 
+  /**
+   * Retry 5XX errors up to three times
+  */
+  private async retryRequest<T>(rq: Observable<T>): Promise<T> {
+    const withRetries = rq.pipe(
+      retry({
+        count: 3,
+        delay: (error) => {
+          if (error instanceof HttpErrorResponse) {
+            const status = error.status;
+            if (status >= 500 && status <= 599) return Promise.resolve();
+          }
+          return Promise.reject(error);
+        },
+      }),
+    );
+    return lastValueFrom(withRetries);
+  }
+
   async search(): Promise<Recipe[]> {
     const url = `${this.baseUrl}/recipe`;
-    const body = await lastValueFrom(this.http.get<any[]>(url, { observe: 'body' }));
+    const rq = this.http.get<any[]>(url, { observe: 'body' });
+    const body = await this.retryRequest(rq);
     return body.map((r) => Recipe.fromObject(r));
   }
 
   async getById(recipeId: string): Promise<Recipe> {
     const url = `${this.baseUrl}/recipe/${recipeId}`;
     try {
-      const body = await lastValueFrom(this.http.get<any>(url, { observe: 'body' }));
+      const rq = this.http.get<any>(url, { observe: 'body' });
+      const body = await this.retryRequest(rq);
       return Recipe.fromObject(body);
     } catch (e) {
       if (e instanceof HttpErrorResponse && e.status === 404) {
